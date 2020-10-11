7815fbff6b03e2e8284bbb32293b91dad94c6597
HyungJunGoo <9h0jun1115@gmail.com>
2020-10-12 01:42:53 +0900

    Make ToDo Save localstorage function

--- challenge/4JS-TODO/lecture/todo.js
+++ challenge/4JS-TODO/lecture/todo.js
@@ -4,20 +4,32 @@ const toDoForm = document.querySelector(".js-toDoForm"),
 
 const TODOS_LS = "toDos";
 
+const toDos = []; // Todo들의  list
+
+function saveToDos() {
+    localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
+}
+
 function paintToDos(text) {
     const li = document.createElement("li");
     
     const delBtn = document.createElement("button");
     delBtn.innerText = "❌";
-    
+    const newId = toDos.length + 1;
     const span = document.createElement("span");
     span.innerText = text;
 
     li.appendChild(span);
     li.appendChild(delBtn);
-
+    li.id = newId;
     toDoList.appendChild(li);
-}
+    const toDoObj = {
+        text: text,
+        id: newId
+    };
+    toDos.push(toDoObj);
+    saveToDos(toDoObj);
+} 
 
 function handleSubmit(event) {
     event.preventDefault();
@@ -27,8 +39,12 @@ function handleSubmit(event) {
 }
 
 function loadToDos() {
-    const toDos = localStorage.getItem(TODOS_LS);
-    if (toDos !== null) {
+    const loadedToDos = localStorage.getItem(TODOS_LS);
+    if (loadedToDos !== null) {
+        const parsedToDos = JSON.parse(loadedToDos);
+        parsedToDos.forEach(function(toDo){
+            paintToDos(toDo.text);
+        })
     }
 }
 
